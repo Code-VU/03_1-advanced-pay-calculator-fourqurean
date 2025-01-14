14e3446680653f9c84b1566b40d0f79301ff3d3a
Christopher Fourqurean <Chris.Fourqurean@vu.com>
2025-01-14 15:05:14 -0600

    first try

--- payCalculator.py
+++ payCalculator.py
@@ -1,7 +1,29 @@
 def calculatePay():
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+
+    # Inputs
+    total_hours = input("Enter total hours: ")
+    total_hours = float(total_hours)
+
+    hourly_rate = input("Enter hourly rate: ")
+    hourly_rate = float(hourly_rate)
+
+    # Logic for overtime pay
+    if total_hours > 40:
+        overtime = total_hours - 40
+        normal_hours = 40
+
+        otpay = overtime * hourly_rate * 1.5
+        regpay = normal_hours * hourly_rate
+
+        gross_pay = otpay + regpay
+
+    # Without overtime    
+    else:
+        gross_pay = total_hours * hourly_rate
     
+    print(gross_pay)
+
     # end assignment
 
 ## if you want to test locally before you try to sync
